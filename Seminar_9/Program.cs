2b08335bf26fce73fcaf5a5df24bc654f6f738bc
Serafima88213 <115182050+Serafima88213@users.noreply.github.com>
2022-11-28 21:26:24 +0300

    DZ

---
+++ Seminar_9/Program.cs
@@ -0,0 +1,79 @@
+/*
+// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+//  Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumber(int start, int end)
+{
+    if(end ==start)
+    {
+        return end.ToString();
+    }
+    return ("start ", "+  PrintNumber(atsrt +1 end));
+}
+Console.WriteLine(PrintNumber(1,n));
+*/
+
+
+/*
+// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumber(int start, int end)
+{
+    if(end <start)
+    {
+        return "введите некоректные числа";
+    }
+    if(end ==start)
+    {
+        return start.ToString();
+    }
+    return (start+ ", "+  PrintNumber(start +1 end));
+}
+Console.WriteLine(PrintNumber(m,n));
+*/
+
+
+/*
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Pow(int n1,int n2)
+{
+    if(n2==0)
+    {
+        return 1;
+    }
+    if(n2==1);
+    {
+       return (n18Pow(n1,n2-1)); 
+    }
+   
+}
+Console.WriteLine(Pow(n1,n2));
+*/
+
+
